524ed0449dc93b2e9dca104d12ef49c28cc988d4
Trey Lowerison <19714082+tlowerison@users.noreply.github.com>
2021-12-21 03:09:21 -0800

    remove unused exported Cardinality struct in repositories/src/lib.rs

--- repositories/src/lib.rs
+++ repositories/src/lib.rs
@@ -30,43 +30,6 @@ pub trait RepositoryError {
     fn not_found() -> Self;
 }
 
-#[derive(Clone, Debug)]
-pub enum Cardinality {
-    One,
-    OneOrNone,
-    AtLeastOne,
-    Many,
-}
-
-pub struct One {}
-pub struct OneOrNone {}
-pub struct AtLeastOne {}
-pub struct Many {}
-
-impl From<One> for Cardinality {
-    fn from(_: One) -> Cardinality {
-        Cardinality::One
-    }
-}
-
-impl From<OneOrNone> for Cardinality {
-    fn from(_: OneOrNone) -> Cardinality {
-        Cardinality::OneOrNone
-    }
-}
-
-impl From<AtLeastOne> for Cardinality {
-    fn from(_: AtLeastOne) -> Cardinality {
-        Cardinality::AtLeastOne
-    }
-}
-
-impl From<Many> for Cardinality {
-    fn from(_: Many) -> Cardinality {
-        Cardinality::Many
-    }
-}
-
 pub trait Transactional {
     type AdaptorError;
 
